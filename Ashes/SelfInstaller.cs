d062c5172bfde4b832918c29d654f8d592d75b21
AmanAshray <ashray.aman@hotstar.com>
2020-09-13 14:42:08 +0530

    Revert "Revert "adding all the initial files""
    This reverts commit 2e39ceb168be299b2c24aeedb0d65cce00e04812.

---
+++ Ashes/SelfInstaller.cs
@@ -0,0 +1,46 @@
+﻿using System.Configuration.Install;
+using System.Reflection;
+
+namespace Ashes
+{
+    public class SelfInstaller
+    {
+        private static readonly string _exePath = Assembly.GetExecutingAssembly().Location;
+        public static bool InstallMe()
+        {
+            bool result;
+            try
+            {
+                ManagedInstallerClass.InstallHelper(new string[] {
+                    SelfInstaller._exePath
+                });
+            }
+            catch
+            {
+                result = false;
+                return result;
+            }
+            result = true;
+            return result;
+        }
+
+        public static bool UninstallMe()
+        {
+            bool result;
+            try
+            {
+                ManagedInstallerClass.InstallHelper(new string[] {
+                    "/u", SelfInstaller._exePath
+                });
+            }
+            catch
+            {
+                result = false;
+                return result;
+            }
+            result = true;
+            return result;
+        }
+
+    }
+}
